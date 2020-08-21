addbd811e72e8c7b8fd0a614fb25da9ef1720445
Hemanth <sherlahemanth001@gmail.com>
2020-08-21 20:10:35 +0530

    Django project

--- wordcount/wordcount/settings.py
+++ wordcount/wordcount/settings.py
@@ -76,9 +76,21 @@ WSGI_APPLICATION = 'wordcount.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'myproject',
+
+        'USER' : 'root',
+
+        'PASSWORD' : '',
+
+        'HOST' : 'localhost',
+
+        'PORT' :3306
+
     }
+
 }
 
 
